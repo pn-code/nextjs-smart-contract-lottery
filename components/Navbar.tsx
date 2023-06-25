09d38ad560c910571671d7e399689ce6df4aa950
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-25 00:05:21 -0700

    Added useEffect to reconnect if recently connected

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,26 @@
 "use client";
+import { useEffect } from "react";
 import { useMoralis } from "react-moralis";
 
 export default function Navbar() {
-  const { enableWeb3, account } = useMoralis();
+  const { enableWeb3, account, isWeb3Enabled } = useMoralis();
+
+  useEffect(() => {
+    if (isWeb3Enabled) return;
+
+    if (typeof window !== "undefined") {
+      if (window.localStorage.getItem("connected")) {
+        enableWeb3();
+      }
+    }
+  }, [isWeb3Enabled]);
+
+  const handleConnectWallet = async () => {
+    await enableWeb3();
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem("connected", "inject");
+    }
+  };
 
   return (
     <nav className="w-full flex justify-between items-center p-2 bg-gray-900 text-white">
@@ -13,13 +31,7 @@ export default function Navbar() {
           Connected to {account.slice(0, 4)}...{account.slice(38)}
         </div>
       ) : (
-        <button
-          onClick={async () => {
-            await enableWeb3();
-          }}
-        >
-          Connect
-        </button>
+        <button onClick={handleConnectWallet}>Connect</button>
       )}
     </nav>
   );
