42dda4d03ecccdbca5828f36064a178d4dc8b5ab
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-25 13:10:01 -0700

    Added Web3UIKit's connect button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,54 +1,11 @@
 "use client";
-import { useEffect } from "react";
-import { useMoralis } from "react-moralis";
+import { ConnectButton } from "@web3uikit/web3";
 
 export default function Navbar() {
-  const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading} =
-    useMoralis();
-
-  // Check for connection and allow reconnect if user recently connected
-  useEffect(() => {
-    if (isWeb3Enabled) return;
-
-    // If window is present, check localStorage for recent connects
-    if (typeof window !== "undefined") {
-      if (window.localStorage.getItem("connected")) {
-        enableWeb3();
-      }
-    }
-  }, [isWeb3Enabled]);
-
-  // Do not allow user to be reprompted by wallet if they choose to disconnect.
-  useEffect(() => {
-    Moralis.onAccountChanged((account) => {
-      console.log(`Account changed to ${account}`);
-      // If the account is null, user disconnected. Deactivate to not reprompt user.
-      if (account == null) {
-        window.localStorage.removeItem("connected");
-        deactivateWeb3();
-        console.log("User has disconnected their wallet.")
-      }
-    });
-  }, []);
-
-  const handleConnectWallet = async () => {
-    await enableWeb3();
-    if (typeof window !== "undefined") {
-      window.localStorage.setItem("connected", "injected");
-    }
-  };
-
   return (
     <nav className="w-full flex justify-between items-center p-2 bg-gray-900 text-white">
       <h1 className="text-xl">Decentralized Lottery</h1>
-
-      {account ? (
-        <div>
-          Connected to {account.slice(0, 4)}...{account.slice(38)}
-        </div>
-      ) : (
-        <button className="bg-indigo-600 font-semibold px-4 py-1 rounded-md text-white hover:bg-indigo-700 disabled:bg-gray-400 disabled:cursor-not-allowed" onClick={handleConnectWallet} disabled={isWeb3EnableLoading}>Connect</button>
-      )}
+      <ConnectButton moralisAuth={false} />
     </nav>
   );
 }
