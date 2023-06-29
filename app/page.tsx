c303d980d0ff75a4da9dac0a7160d45ba75e195e
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-29 14:49:37 -0700

    Added new sections to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
+import AboutSection from "@/components/AboutSection";
+import Footer from "@/components/Footer";
 import LotterySection from "@/components/LotterySection";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <main>
+        <main className="h-[calc(100vh+90px)] sm:h-screen w-full bg-gray-100">
             <Navbar />
+            <AboutSection />
             <LotterySection />
+            <Footer />
         </main>
     );
 }
