07a1c86400920fe74492de88080f60d2ebd6dd1c
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-29 14:49:17 -0700

    Updated lottery title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import { ConnectButton } from "@web3uikit/web3";
 export default function Navbar() {
   return (
     <nav className="w-full flex justify-between items-center p-2 bg-gray-900 text-white">
-      <h1 className="text-xl">Decentralized Lottery</h1>
+      <h1 className="text-xl font-semibold">Ethraffle</h1>
       <ConnectButton moralisAuth={false} />
     </nav>
   );
