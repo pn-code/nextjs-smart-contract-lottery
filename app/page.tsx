0d04fd3791214161b1e165b5674cfd9a5345db34
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-29 21:09:32 -0700

    Updated spacing to be more mobile-friendly

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <main className="h-[calc(100vh+90px)] sm:h-screen w-full bg-gray-100">
+        <main className="h-[calc(100vh+320px)] sm:h-[calc(100vh-60px)] w-full bg-gray-100">
             <Navbar />
             <AboutSection />
             <LotterySection />
